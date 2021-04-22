aae797c87ba779a593ebbb907cb3b9f2458ef9a6
kosuke-17 <tamutamu17@icloud.com>
2021-04-22 21:00:51 +0900

    データベースのデータ表示

--- routes/hello.js
+++ routes/hello.js
@@ -1,30 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const http = require('https');
-const parseString = require('xml2js').parseString;
 
+const sqlite3 = require('sqlite3');
 
+// データベースオブジェクトの取得
+const db = new sqlite3.Database('mydb.sqlite3');
+
+// GETアクセスの処理
 router.get('/', (req, res, next) => {
-  var opt = {
-    host: 'news.google.com',
-    port: 443,
-    path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
-  };
-  http.get(opt, (res2) => {
-    var body ='';
-    res2.on('data', (data) => {
-      body += data;
-    });
-    res2.on('end', () => {
-      parseString(body.trim(), (err, result) => {
-        console.log(result);
+  db.serialize(() => {
+    // レコードをすべて取り出す
+    db.all("select * from mydata",(err, rows) => {
+      // データベースアクセス完了の処理
+      if (!err) {
         var data = {
-          title: 'Google News',
-          content: result.rss.channel[0].item
+          title: 'Hello!',
+          content: rows
         };
         res.render('hello', data);
-      });
-    })
+      }
+    });
   });
 });
 
