27f466acd3b2c2546275f987b9d34a8cc38fee9f
kosuke-17 <tamutamu17@icloud.com>
2021-04-25 14:49:39 +0900

    ユーザー検索機能実装

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models/index');
+
+const { Op } = require("sequelize");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/',(req, res, next)=> {
+  const min = req.query.min * 1
+  const max = req.query.max * 1
+  db.User.findAll({
+    where: {
+      age: {[Op.gte]:min, [Op.lte]:max}
+    }
+  }).then(usrs => {
+    var data = {
+      title: 'Users/Index',
+      content: usrs
+    }
+    res.render('users/index', data);
+  });
 });
 
 module.exports = router;
