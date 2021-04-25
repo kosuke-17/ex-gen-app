6086736b6a0538db50f51ff94906501d8b050672
kosuke-17 <tamutamu17@icloud.com>
2021-04-25 16:24:20 +0900

    ユーザモデル編集

--- models/user.js
+++ models/user.js
@@ -1,10 +1,31 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    pass: DataTypes.STRING,
-    mail: DataTypes.STRING,
-    age: DataTypes.INTEGER
+    name: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    },
+    pass: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    },
+    mail: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: true,
+        min: 0
+      }
+    }
   },  {});
   User.associate = function(modules) {
     // associations can be defined here
